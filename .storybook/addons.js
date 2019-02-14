fabf6603bdbd4535204f55ecb9867e81c5050acb
goooseman <goooseman@me.com>
2019-02-14 19:18:14 +0200

    feat(storybook): ✨  Background addon with Trucknet'\''s settings

--- .storybook/addons.js
+++ .storybook/addons.js
@@ -5,3 +5,4 @@ import "@storybook/addon-knobs/register";
 import "@storybook/addon-viewport/register";
 import "@storybook/addon-actions/register";
 import "@storybook/addon-a11y/register";
+import "@storybook/addon-backgrounds/register";
--- .storybook/config.js
+++ .storybook/config.js
@@ -8,15 +8,36 @@ import { checkA11y } from "@storybook/addon-a11y";
 import { withKnobs } from "@storybook/addon-knobs";
 import { withSmartKnobs } from "storybook-addon-smart-knobs";
 import "@storybook/addon-console";
+import { withBackgrounds } from "@storybook/addon-backgrounds";
 
 addDecorator(
   withInfo({
     inline: false,
   }),
 );
+addDecorator(
+  withBackgrounds([
+    { name: "white", value: "#fff", default: true },
+    { name: "Lime Green", value: "#2CCE62" },
+    { name: "Tarawera", value: "#29464D" },
+    { name: "Elephant", value: "#1F3239" },
+    { name: "Nero", value: "#212121" },
+    { name: "Night Rider", value: "#303030" },
+    { name: "Mortar", value: "#535353" },
+    { name: "Grey", value: "#808080" },
+    { name: "Very Light Grey", value: "#C7C7C7" },
+    { name: "White Smoke", value: "#EBEBEB" },
+    { name: "Dodger Blue", value: "#2979FF" },
+    { name: "Lightning Yellow", value: "#F9A825" },
+    { name: "Torch Red", value: "#FF1744" },
+  ]),
+);
+
 addDecorator(withSmartKnobs);
 addDecorator(withKnobs);
 addDecorator(checkA11y);
+
+theme.palette.background.default = "none"; // Need for background plugin
 addDecorator((story) =>
   React.createElement(MuiThemeProvider, {
     theme,
